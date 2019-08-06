12eec91c73cc0ef50e51d850b24f2e32afdef820
Alekspot <aleksandrpotkin12@mail.ru>
2019-08-06 15:25:17 +0300

    Начал делать input

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,16 +1,20 @@
 <template>
-    <h1 class="red">Hellow World</h1>
+    <div class="app">
+        <h1 class="red">Hellow World</h1>
+        <BlogInput></BlogInput>
+    </div>  
 </template>
 <script>
+import BlogInput from './BlogInput.vue'
+
 export default {
-    
+    components:{
+        BlogInput
+    }
 }
 </script>
 <style lang="less">
-   h1{
-       color:rgb(25, 209, 65)
-   }
- h1{
-      font-size: 31px;
-   }
+  .app{
+      box-sizing: border-box
+  }
 </style>
