f5d2349c1fb540603f9622b4e3e5d186dd529f17
Alekspot <aleksandrpotkin12@mail.ru>
2019-08-09 11:35:18 +0300

    Реализовано добавление картинок

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,7 +2,8 @@
     <ul class="list">
         <li   v-for="(post,index) in postList" class="list__item" :key="index">
             <div class="list__post">
-                <h3>{{post.dt}}</h3>
+                <h3>{{post.dt|dateFormat}}</h3>
+                <img class="list__img" v-for="img in imgFilter(post.img)" :key="img"  :src="'../uploads/' + img" alt="">
                 <p v-html="post.text"></p>   
             </div>
             <div class="list__actions">
@@ -25,7 +26,40 @@ export default {
     methods:{
         deletePost(id){
             this.$store.dispatch('deletePost', id);
+        },
+        imgFilter:function (imgStr) {
+            if(!imgStr) return false
+            else return imgStr.split(':');    
         }
+    },
+    filters:{
+        dateFormat:function(dt){
+            let date = dt.substring(0,10);
+            let time = dt.substring(11,16);
+            var monthNames = [
+                "Января", "Февраля", "Марта",
+                "Апреля", "Мая", "Июня", "Июля",
+                "Августа", "Сентября", "Октября",
+                "Ноября", "Декабря"
+            ];
+            let monthId; 
+                      
+            let arr = date.split('-');
+           // return arr[1][0];
+            if(arr[1][0]=='0'){
+                monthId = +arr[1][1]
+            } else monthId = + arr[1];
+
+            let day;
+            if(arr[2][0]=='0'){
+                day = +arr[2][1];
+            } else day = + arr[2];
+            
+            let month = monthNames[monthId-1];
+            let year = arr[0];
+            
+           return `${day}  ${month}  ${year}`;
+        }    
     }
 }
 </script>
@@ -41,7 +75,10 @@ export default {
         min-height: 80px;
     }
     .list__post {
-        flex:1
+        flex:1;
+    }
+    .list__img {
+        max-width: 200px;
     }
     .list__actions {
         margin-right: 30px
